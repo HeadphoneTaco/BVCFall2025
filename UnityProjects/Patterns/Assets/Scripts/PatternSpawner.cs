748f8242418a424c7e7aed832ae0aa5aa153f845
HeadphoneTaco <michael.hardwicke92@gmail.com>
2025-09-19 18:08:54 -0600

    Pseudocode scaffolding

---
+++ UnityProjects/Patterns/Assets/Scripts/PatternSpawner.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class PatternSpawner : MonoBehaviour
+{
+    
+    //list of patterns to choose from
+    //list of prefabs to choose from
+
+    //how many prefabs to spawn
+    //how much space between each prefab
+    
+    void Start()
+    {
+       
+    }
+    
+    void Update()
+    {
+        
+    } 
+    
+    //listen for button presses to change prefab and pattern
+
+   
+
+    //change patterns
+
+    //change prefabs
+
+    // remove previously spawned objects
+
+    //get info from currently selected pattern
+
+    //spawn new objects based on pattern
+    
+
+//update ui
+}

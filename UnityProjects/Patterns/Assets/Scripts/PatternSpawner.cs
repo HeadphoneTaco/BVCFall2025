4da8b420eb218e7b7ad1c95c9fe1c6b4a6b22aab
HeadphoneTaco <michael.hardwicke92@gmail.com>
2025-09-19 18:47:30 -0600

    I think this makes sense

--- UnityProjects/Patterns/Assets/Scripts/PatternSpawner.cs
+++ UnityProjects/Patterns/Assets/Scripts/PatternSpawner.cs
@@ -1,52 +1,103 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PatternSpawner : MonoBehaviour
 {
-    
-    //list of patterns to choose from
-    //list of prefabs to choose from
+    [Header("Prefab Settings")]
+    public List<GameObject> prefabs; //list of prefabs to choose from
 
-    //how many prefabs to spawn
-    //how much space between each prefab
-    
+    public int count; //how many prefabs to spawn
+    public float spacing; //how much space between each prefab
+
+    [Header("Pattern Settings")]
+    public List<SpawnPatternSO> patterns; //list of scriptable pattern oooobjectss to choose from
+    //TODO: fix this
+
+    private int _currentPatternIndex;
+    private int _currentPrefabIndex;
+    //keep track of spawned objects to nuke em later
+    private List<GameObject> _spawnedObjects = new();
+
     void Start()
     {
-       
+        SpawnCurrentPattern();
     }
     //have a default pattern and prefab to spawn on start
+
+    //listen for button presses to change prefab and pattern
     
-    
+    //change this to switch case for efficiency?
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            NextPattern();
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            PreviousPattern();
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            PreviousPrefab();
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            NextPrefab();
+        }
+    }
+    //next pattern
+    public void NextPattern()
+    {
+        _currentPatternIndex = (_currentPatternIndex + 1) % patterns.Count;
+        SpawnCurrentPattern();
+    }
+    //previous pattern
+    public void PreviousPattern()
+    {
+        _currentPatternIndex = (_currentPatternIndex - 1 + patterns.Count) % patterns.Count;
+        SpawnCurrentPattern();
+    }
+    //next prefab
+    public void NextPrefab()
+    {
+        _currentPrefabIndex = (_currentPrefabIndex + 1) % patterns.Count;
+        SpawnCurrentPattern();
+    }
+    //previous prefab
+    public void PreviousPrefab()
+    {
+        _currentPrefabIndex = (_currentPrefabIndex - 1 + patterns.Count) % patterns.Count;
+        SpawnCurrentPattern();
+    }
+
+
+    //Change pattern or prefab
+    private void SpawnCurrentPattern()
+    {
+        // 'unalive' previously spawned objects
+        foreach (var obj in _spawnedObjects)
+        {
+            Destroy(obj);
+        }
+        _spawnedObjects.Clear();
+
+        //get new info
+        var positions = patterns[_currentPatternIndex].GetPositions(count, spacing);
+
+        //spawn new objects based on pattern
+        foreach (var pos in positions)
+        {
+            var obj = Instantiate(prefabs[_currentPrefabIndex], pos, Quaternion.identity, transform);
+            _spawnedObjects.Add(obj);
+        }
         
-    } 
-    
-    //listen for button presses to change prefab and pattern
-
-   
-
-    
-    
-    
-    
-    //change prefab
-            // remove previously spawned objects
-
-            //get info from currently selected pattern
-
-            //spawn new objects based on pattern
-            
-
-    //Change pattern    
-    
-            // remove previously spawned objects
-
-            //get info from currently selected pattern
-
-            //spawn new objects based on pattern
-    
-
-//update ui
-
-//error handling where?
+    }
+    //update ui
 }
+
+    //error handling where?
+

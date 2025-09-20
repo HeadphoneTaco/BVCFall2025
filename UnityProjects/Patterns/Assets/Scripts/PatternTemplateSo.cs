7ea1d4a826e5e6a3d606a95083a99707c83cbe75
HeadphoneTaco <michael.hardwicke92@gmail.com>
2025-09-19 22:34:18 -0600

    Create Template Scriptable Object

---
+++ UnityProjects/Patterns/Assets/Scripts/PatternTemplateSo.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Spawn Patterns/Template")]
+public class PatternTemplateSo : SpawnPatternSo
+{
+    [Header("Custom Settings")]
+    public float somethinggoeshere;
+
+    //override the abstract
+    public override List<Vector3> GetPositions(int count, float spacing)
+    {
+        //make list of new positions
+        var positions = new List<Vector3>();
+
+        //make sure that there are enough prefabs to fill the pattern
+        //since this is a ____,
+        for (int i = 0; i < count; i++)
+        {
+            // iterate through ____
+            float x = i * spacing;
+            float y = 0; 
+            float z = 0;
+
+            //add new positions to list
+            positions.Add(new Vector3(x, y, z));
+        }
+
+        return positions;
+    }
+}

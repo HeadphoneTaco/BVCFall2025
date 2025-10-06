fe8f062b1f7e9087b346bd0944e604fdde64e8f5
HeadphoneTaco <michael.hardwicke92@gmail.com>
2025-10-06 15:11:52 -0600

    Color and foldout for variations in editor

--- UnityProjects/Patterns/Assets/Scripts/Variations/Variation.cs
+++ UnityProjects/Patterns/Assets/Scripts/Variations/Variation.cs
@@ -9,7 +9,11 @@ namespace FractalVariations
     public abstract class Variation : ScriptableObject
     {
         [Range(0f, 2f)] public float weight = 1f;
+        [ColorUsage(true, true)] public Color previewColor = Color.cyan;
 
+#if UNITY_EDITOR
+        [HideInInspector] public bool editorFoldout = true;
+#endif
         /// <summary>
         /// Apply the variation's transformation to the input point.
         /// </summary>
